1c6a421b2f843dad627c7c4286e0d2e4f78a10ca
Ben Meiri <ben@codefresh.io>
2016-07-07 17:38:59 +0300

    Update itest.sh

--- itest.sh
+++ itest.sh
@@ -4,4 +4,5 @@ echo "Starting test"
 # In the context of an integration test, all containers create in the composition are prefixed with `integration_` and suffixed by the container's index
 docker run -it --link integration_web_1:web phusion/baseimage bash -c "curl --silent \"http://web:8000\" > /dev/null"
 
+
 exit=$?
